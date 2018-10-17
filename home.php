32959de8da889df07a45e043ac2f588ab6f1febd
tirthankar12121997 <b116062@iiit-bh.ac.in>
2018-10-17 15:59:40 +0530

    changed the theme of the app and added home page

---
+++ home.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>Welcome!</title>
+    <meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <style>
+	body {
+		color: #ffffff;
+	}
+	.hbg {
+		background-color: #262626;
+		color: #ffffff;
+	}
+	.hindent {
+		text-indent: 30px;
+	}
+	.po {
+		background-color: #e65c00;
+		cursor: pointer;
+	}
+	.po:hover {
+		box-shadow: 3px 5px 10px #888888;
+	}
+  </style>
+  <body>
+	<div class="container-fluid hbg">
+		<h1 class="display-1 hindent">Online Classroom</h1>
+	</div>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>                        
+				</button>
+				<a class="navbar-brand" href="#">StayConnected</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li class="dropdown"><a href="#">Join Group</a></li>
+					<li><a href="#">Create Group</a></li>
+					<li><a href="#"><i class="glyphicon glyphicon-user"></i> Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- home section -->
+	<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3">
+		<div class="panel po">
+			<div class="panel-heading">
+				<h3 class="display-2">Group - 1</h3>
+			</div>
+			<div class="panel-body">
+				Group - 1 description
+			</div>
+		</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="panel po">
+			<div class="panel-heading">
+				<h3 class="display-2">Group - 2</h3>
+			</div>
+			<div class="panel-body">
+				Group - 2 description
+			</div>
+		</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="panel po">
+			<div class="panel-heading">
+				<h3 class="display-2">Group - 3</h3>
+			</div>
+			<div class="panel-body">
+				Group - 3 description
+			</div>
+		</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="panel po">
+			<div class="panel-heading">
+				<h3 class="display-2">Group - 4</h3>
+			</div>
+			<div class="panel-body">
+				Group - 4 description
+			</div>
+		</div>
+		</div>
+	</div>
+	</div>
+	
+	<!-- join group section -->
+	
+	
+	<!-- create group section -->
+	
+	
+  </body>
+</html>
+  
